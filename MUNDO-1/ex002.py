6c82d2e1b1bec870fe911cae0f38405c8f4bfd6f
Guilherme Camurça Chagas <153216205+guiOchagas@users.noreply.github.com>
2024-10-02 02:48:52 -0300

    Delete MUNDO-1 directory

--- MUNDO-1/ex002.py
+++
@@ -1,2 +0,0 @@
-nome = str(input('Digite o seu nome: ')
-print(f'Seja muito bem vindo {nome}')
--- MUNDO-1/ex003.py
+++
@@ -1,4 +0,0 @@
-n1 = int(input('Digite um valor: '))
-n2 = int(input('Digite outro valor: '))
-result = n1 + n2
-print(f'O resultado de {n1} + {n2} é igual a {result}')
