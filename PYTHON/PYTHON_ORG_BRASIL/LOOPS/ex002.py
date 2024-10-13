bc5d964a6e21d5de6121b0e28b73284cb4b43a2f
Guilherme Camurça Chagas <153216205+guiOchagas@users.noreply.github.com>
2024-10-13 01:56:30 -0300

    Create ex002.py

---
+++ PYTHON/PYTHON_ORG_BRASIL/LOOPS/ex002.py
@@ -0,0 +1,11 @@
+# Faça um programa que leia um nome de usuário e a sua senha e não aceite a senha igual ao nome do usuário, mostrando uma mensagem de erro e voltando a pedir as informações.
+
+login = str(input('Login: '))
+
+while True:
+    password = str(input('Senha: '))
+    if login == password:
+        print('O login e senha não podem ser iguais. Escolha outra senha.')
+    else:
+        print('ENTRANDO...')
+        break
