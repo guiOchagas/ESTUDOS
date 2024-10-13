30b27d0d37681406d11fbaf2e3af5d791fff1cae
Guilherme Camurça Chagas <153216205+guiOchagas@users.noreply.github.com>
2024-10-13 01:48:01 -0300

    Delete PYTHON/PYTHON_ORG_BRASIL/ex001.py

--- PYTHON/PYTHON_ORG_BRASIL/ex001.py
+++
@@ -1,9 +0,0 @@
-nota = 0
-
-while True:
-    try:
-        nota = int(input('Digite um número: '))
-        print(f'Você digitou o número {nota}')
-        break
-    except:
-        print('Você digitou um valor inválido, por favor digite um número inteiro.')
