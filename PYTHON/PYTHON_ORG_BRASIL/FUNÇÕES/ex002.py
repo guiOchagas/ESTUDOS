a1120f4f3d574e735479dd07c9c74ec1efc10b1a
Guilherme Camurça Chagas <153216205+guiOchagas@users.noreply.github.com>
2024-10-24 01:25:55 -0300

    Create ex002.py

---
+++ PYTHON/PYTHON_ORG_BRASIL/FUNÇÕES/ex002.py
@@ -0,0 +1,10 @@
+"""Faça um programa, com uma função que necessite de três argumentos, e que forneça a soma desses três argumentos."""
+
+
+def soma(a, b, c):
+    total = a + b + c
+    return total
+    
+    
+somando = soma(2, 3, 4)
+print(somando)
