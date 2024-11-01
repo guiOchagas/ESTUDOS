09cda1348ce8596052f7428913cd6402ffdac4b3
Guilherme Camurça Chagas <153216205+guiOchagas@users.noreply.github.com>
2024-10-31 22:37:49 -0300

    Update ex008.py

--- PYTHON/PYTHON_ORG_BRASIL/CLASSES/ex008.py
+++ PYTHON/PYTHON_ORG_BRASIL/CLASSES/ex008.py
@@ -1,18 +1,21 @@
 """Classe Macaco: Desenvolva uma classe Macaco,que possua os atributos nome e bucho (estomago) e pelo menos os métodos comer(), verBucho() e digerir(). Faça um programa ou teste interativamente, criando pelo menos dois macacos, alimentando-os com pelo menos 3 alimentos diferentes e verificando o conteúdo do estomago a cada refeição. Experimente fazer com que um macaco coma o outro. É possível criar um macaco canibal?"""
 
 
-
 class Macaco():
     def __init__ (self, nome, bucho):
         self.nome = nome
         self.bucho = bucho
         
     def comer(self, comida):
-        print(f'O {self.nome} comeu {comida.nome} e consumiu {comida.kcal} calorias.')
-        self.bucho += comida.kcal
+        if isinstance(comida, Macaco):
+            print('Não é possível comer um macaco.')
+        else:
+            print(f'O {self.nome} comeu {comida.nome} e consumiu {comida.kcal} calorias.')
+            self.bucho += comida.kcal
     
     def verBucho(self):
-        pass
+        print(f'O estomago do {self.nome} está com {self.bucho}')
+    
     
     def digerir(self):
         pass
@@ -41,10 +44,19 @@ class Maca(Comida):
 
 
 
-macaco = Macaco('Cesar', 0)
+macaco1 = Macaco('Cesar', 0)
+macaco2 = Macaco('Gui', 0)
+
 banana = Banana()
 
-macaco.comer(banana)
+morango = Morango()
 
+maca = Maca()
 
-
+macaco1.comer(banana)
+macaco2.comer(morango)
+print()
+macaco1.verBucho()
+macaco2.verBucho()
+print()
+macaco1.comer(macaco2)
